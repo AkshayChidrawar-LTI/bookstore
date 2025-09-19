3c4ee404d77e13ab66157fc636a4999306ec8392
AkshayChidrawar-LTI <AkshayChidrawar-LTI>
2025-09-19 16:35:46 +0000

    commit

--- dependencies/Helpers.py
+++ dependencies/Helpers.py
@@ -186,7 +186,3 @@ except Exception as e:
 """
     save_script(object_ddl,script)
     print(f"\nBelow script generated to DROP {object_type} '{object_name}'. \n'{object_ddl}'")
-
-# COMMAND ----------
-
-#print
